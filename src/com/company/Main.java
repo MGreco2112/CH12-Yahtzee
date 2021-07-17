d1c00368e10278eda087085fd7122d96b8e5113e
MGreco2112 <mgreco2112@tutanota.com>
2021-07-16 20:47:33 -0400

    reworked so last rounds dice carry over to start of next round

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,23 +10,37 @@ public class Main {
 
         boolean isLoser = false;
         int winStreak = 0;
+        boolean initialDie1Roll = true;
+        boolean initialDie2Roll = true;
+        int lastRoundDie1 = 0;
+        int lastRoundDie2 = 0;
 
         while (!isLoser) {
             Scanner scanner = new Scanner(System.in);
             Die die1 = new Die();
             Die die2 = new Die();
+            int die1Roll;
+            int die2Roll;
 
-            die1.roll();
-            die2.roll();
+            if (initialDie1Roll && initialDie2Roll) {
+                die1Roll = die1.roll();
+                die2Roll = die2.roll();
+            } else {
+                die1Roll = lastRoundDie1;
+                die2Roll = lastRoundDie2;
+            }
 
-            int oldTotal = die1.faceUpValue + die2.faceUpValue;
+            int oldTotal = die1Roll + die2Roll;
 
-            System.out.println(die1.faceUpValue + " + " + die2.faceUpValue + " = " + oldTotal);
+            System.out.println(die1Roll + " + " + die2Roll + " = " + oldTotal);
             System.out.println("Will the next number be (h)igher or (l)ower?");
             String input = scanner.next();
 
             die1.roll();
             die2.roll();
+
+
+
 
             int total = die1.faceUpValue + die2.faceUpValue;
 
@@ -37,9 +51,13 @@ public class Main {
 
             boolean isLarger = total > oldTotal;
             System.out.println(die1.faceUpValue + " + " + die2.faceUpValue + " = " + total);
+            lastRoundDie1 = die1.faceUpValue;
+            lastRoundDie2 = die2.faceUpValue;
 
-            if (input == "h") {
+            if (input.equals("h")) {
                 if (isLarger) {
+                    initialDie1Roll = false;
+                    initialDie2Roll = false;
                     winStreak++;
                     System.out.println("You win! Current streak: " + winStreak);
                 } else {
@@ -51,6 +69,8 @@ public class Main {
                     System.out.println("You lose! Total streak: " + winStreak);
                     isLoser = true;
                 } else {
+                    initialDie1Roll = false;
+                    initialDie2Roll = false;
                     winStreak++;
                     System.out.println("You win! Current streak: " + winStreak);
                 }
