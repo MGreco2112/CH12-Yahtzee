336355019b3081ca2fefdbf395e82c5a4045237e
MGreco2112 <mgreco2112@tutanota.com>
2021-07-19 20:58:24 -0400

    Created cup class, added methods

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,35 @@
 package com.company;
 
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        
+        Cup cup = new Cup();
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+
+        cup.roll();
+
+        System.out.println(cup.displayCup());
+
+        System.out.println("Which dice do you want to reroll? (1-5)");
+
+        input = scanner.nextLine();
+
+        cup.roll(cup.parseSelections(input));
+
+        System.out.println(cup.displayCup());
+
+        System.out.println("Which dice do you want to reroll? (1-5)");
+
+        input = scanner.nextLine();
+
+        cup.roll(cup.parseSelections(input));
+
+        System.out.println(cup.displayCup());
     }
 }
