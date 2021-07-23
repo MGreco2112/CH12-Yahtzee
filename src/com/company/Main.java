f72bffb1c77fcbdfb78ed9c73d5724e4f435e022
MGreco2112 <mgreco2112@tutanota.com>
2021-07-22 21:17:15 -0400

    Added HashMap main stuff

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,11 +2,27 @@ package com.company;
 
 
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class Main {
 
     public static void main(String[] args) {
+        Map<Integer, Integer> freq = new TreeMap<>();
+        System.out.println(freq);
+
+        Cup cup = new Cup();
+        cup.roll();
+        for (Die die : cup.dice) {
+            if (!freq.containsKey(die.faceUpValue)) {
+                freq.put(die.faceUpValue, 0);
+            }
+            freq.put(die.faceUpValue, freq.get(die.faceUpValue) + 1);
+        }
+        System.out.println(cup.displayCup());
+        System.out.println(freq);
+
         Yahtzee game = new Yahtzee();
         game.play();
 
