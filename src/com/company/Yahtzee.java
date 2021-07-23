baa93efd3bcfa6bd94e718058fdcc95ae9ec3860
MGreco2112 <mgreco2112@tutanota.com>
2021-07-22 20:47:23 -0400

    Added multiple player system and modified the scoring system

--- src/com/company/Yahtzee.java
+++ src/com/company/Yahtzee.java
@@ -1,48 +1,102 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Yahtzee {
     private final Scanner scanner = new Scanner(System.in);
 //    public Cup cup = new Cup();
     public Player player;
+    public ArrayList<Player> players = new ArrayList<>();
+    private final int ROUND = 5;
+    private final int MAXPLAYERS = 6;
+    private final int MINPLAYER = 1;
 
     public Yahtzee() {
-        System.out.println("Please enter your name");
-        String name = scanner.nextLine().trim();
-        this.player = new Player(name);
+        System.out.println("How many will be playing Yahtzee?");
+        int numberPlayers;
+        do {
+            numberPlayers = scanner.nextInt();
+        }
+        while (numberPlayers < MINPLAYER || numberPlayers > MAXPLAYERS);
+
+        scanner.nextLine();
+
+        while (players.size() < numberPlayers) {
+            System.out.println("Please enter your name");
+            String name = scanner.nextLine().trim();
+            players.add(new Player(name));
+        }
+
     }
 
 
-    public void getSelections() {
+    public void getSelections(Player activePlayer) {
 
         System.out.println("Which dice do you want to re-roll? (0-5)");
 
         String input = scanner.nextLine();
 
-        player.cup.roll(player.cup.parseSelections(input));
-    }
-
-    public void turn() {
-        player.cup.roll();
-
-        for (int i = 0; i < 2; i++) {
-            System.out.println(player.cup.displayCup());
-            getSelections();
+        if (input.equals("")) {
+            return;
         }
 
-        System.out.println(player.cup.displayCup());
+        activePlayer.cup.roll(activePlayer.cup.parseSelections(input));
+    }
+
+    public void turn(Player activePlayer) {
+        System.out.println(activePlayer.name + "'s turn, press Enter to begin");
+        scanner.nextLine();
+        activePlayer.cup.roll();
+        int reroll = 2;
+
+        for (int i = 0; i < reroll; i++) {
+            System.out.println(activePlayer.cup.displayCup());
+            getSelections(activePlayer);
+        }
+
+        System.out.println(activePlayer.cup.displayCup());
         System.out.println("Round Score:");
-        System.out.println(player.updateScore());
+        System.out.println(activePlayer.name + " scored " + activePlayer.updateScore());
         System.out.println("Next Round!");
+    }
+
+    private void round() {
+        for (Player activePlayer : players) {
+            turn(activePlayer);
+        }
+    }
+
+    private void displayResults() {
+        Player currentWinner = players.get(0);
+
+//        for (int i = 1; i < players.size(); i++) {
+//            if (currentWinner.score < players.get(i).score) {
+//                currentWinner = players.get(i);
+//            }
+//        }
+//
+        for (Player player : players) {
+            if (currentWinner.score < player.score) {
+                currentWinner = player;
+            }
+            System.out.printf("%s scored %d\n", player.name, player.score);
+        }
+        System.out.printf(
+                "%s is the winner with %d points!!\n",
+                currentWinner.name,
+                currentWinner.score
+        );
+
     }
 
     public void play() {
         int turnCount = 0;
-        while (turnCount < 5) {
-            turn();
+        while (turnCount < ROUND) {
+            round();
             turnCount++;
         }
-        System.out.println(player.name + " ended with a score of " + player.score + " points!\nGame Over");
+        displayResults();
+
     }
 }
